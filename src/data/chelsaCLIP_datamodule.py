e72537226b09ea78c2e6617c92c8e1ca602652d3
jdollinger-bit <jdollinger@uzh.ch>
2025-01-17 15:36:20 +0100

    such a mess lol

--- src/data/chelsaCLIP_datamodule.py
+++ src/data/chelsaCLIP_datamodule.py
@@ -4,6 +4,11 @@ import torch
 from lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
 from torchvision.transforms import transforms
+from tqdm import tqdm
+import numpy as np
+
+from src.data.chelsaCLIP_dataset import ChelsaCLIPDataset
+from src.data.world_sampler import WeightedRandomWorldSampler
 
 
 class ChelsaCLIPDataModule(LightningDataModule):
@@ -46,10 +51,19 @@ class ChelsaCLIPDataModule(LightningDataModule):
 
     def __init__(
         self,
-        climatology_dir: str = "data/",
-        batch_size: int = 64,
+        climatology_dir: str,
+        input_dir: str,
+        batch_size: int,
         num_workers: int = 0,
         pin_memory: bool = False,
+        skip_samples: int = None,
+        use_all_for_training: bool = False,
+        return_size: int = 1,
+        provide_chelsa_similarity_matrix: bool = False,
+        local_multi_sampling: bool = False,
+        sampler: str = "default",
+        whiten_with_pca: bool = False,
+        months = "march",
     ) -> None:
         """Initialize a `ChelsaCLIPDataModule`.
 
@@ -74,7 +88,45 @@ class ChelsaCLIPDataModule(LightningDataModule):
         self.data_val: Optional[Dataset] = None
         self.data_test: Optional[Dataset] = None
 
+        self.monthly_arrays = None
+
         self.batch_size_per_device = batch_size
+
+        def collate_fn(batch):
+            if provide_chelsa_similarity_matrix:
+                if local_multi_sampling:
+                    lonlat = torch.cat([x[0] for x in batch], 0)
+                    months = torch.cat([x[1] for x in batch], 0)
+                    chelsa = torch.cat([x[2] for x in batch], 0)
+                else:
+                    lonlat = torch.stack([torch.tensor(x[0]) for x in batch])
+                    months = torch.stack([torch.tensor(x[1]) for x in batch])
+                    chelsa = torch.stack([torch.tensor(x[2]) for x in batch])
+                norm = torch.norm(chelsa, dim=1).view(-1,1).type(torch.float)
+                similarity = torch.mm(chelsa.type(torch.float),chelsa.T.type(torch.float)) / torch.mm(norm, norm.T)
+                if isinstance(provide_chelsa_similarity_matrix, float):
+                    # The case where we pass a cutoff
+                    similarity = (similarity > provide_chelsa_similarity_matrix).type(torch.float)
+                    similarity = similarity * 2 - 1
+                #total = len(chelsa) * len(chelsa)
+                #print("Total is", total)
+                #pos_samples = len(similarity) * len(similarity) + similarity.sum()
+                #sim = (similarity > 0.999999).sum()
+                #print("Similarity cutoff 0.999999", sim, "->", (sim/total)*100, "percent")
+                return lonlat, months, chelsa, similarity
+            elif local_multi_sampling:
+                lonlat = torch.cat([x[0] for x in batch], 0)
+                months = torch.cat([x[1] for x in batch], 0)
+                chelsa = torch.cat([x[2] for x in batch], 0)
+                return lonlat, months, chelsa
+            else:
+                lonlat = torch.stack([torch.tensor(x[0]) for x in batch])
+                months = torch.stack([torch.tensor(x[1]) for x in batch])
+                chelsa = torch.stack([torch.tensor(x[2]) for x in batch])
+                #chelsa = torch.stack([x[1] for x in batch])
+                return lonlat, months, chelsa
+
+        self.collate_fn = collate_fn
 
     def prepare_data(self) -> None:
         """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
@@ -96,29 +148,97 @@ class ChelsaCLIPDataModule(LightningDataModule):
 
         :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
         """
-        # Divide batch size by the number of devices.
-        if self.trainer is not None:
-            if self.hparams.batch_size % self.trainer.world_size != 0:
-                raise RuntimeError(
-                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
-                )
-            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+        if self.monthly_arrays is None:
+            #self.var_names = ["clt", "cmi", "hurs", "pet", "pr", "rsds", "sfcWind", "tas", "tasmax", "tasmin", "vpd"]
+            if self.hparams.months == "all":
+                self.months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+            elif self.hparams.months == "seasons":
+                self.months = ["03", "06", "09", "12"]
+            elif self.hparams.months == "march":
+                self.months = ["03"]
+            #self.months = ["03", "09"]
+            #self.months = ["03"]
+            self.monthly_arrays = {}
+            print("Loading monthly rasters")
+            for month in tqdm(self.months):
+                #self.monthly_arrays[month] = np.load(self.hparams.climatology_dir + month + "_monthly_float16.npy")#, mmap_mode='r')
+                self.monthly_arrays[month] = np.load(self.hparams.climatology_dir + month + "_monthly_float16_land_only.npy")
 
         # load and split datasets only if not loaded already
-        if stage == "fit" and not self.data_train:
-            self.data_train = 
+        if (stage == "validate" or stage == "fit" or stage == "test") and not self.data_train:
+            self.data_train = ChelsaCLIPDataset(
+                monthly_arrays=self.monthly_arrays,
+                land_coordinates_file=self.hparams.input_dir+("land_coordinates.npy" if self.hparams.use_all_for_training else "land_coordinates_train.npy"),
+                point_to_coord_file=self.hparams.input_dir+("point_to_coord.npy" if self.hparams.use_all_for_training else "point_to_coord_train.npy"),
+                months=self.months,
+                skip_samples = self.hparams.skip_samples,
+                return_size=self.hparams.return_size,
+                local_multi_sampling=self.hparams.local_multi_sampling,
+                whiten_with_pca=self.hparams.whiten_with_pca,
+            )
+
+        if (stage == "validate" or stage == "fit") and not self.data_val:
+            """self.data_val = ChelsaCLIPDataset(
+                monthly_arrays=self.monthly_arrays,
+                land_coordinates_file=self.hparams.input_dir+"land_coordinates_val.npy",
+                point_to_coord_file=self.hparams.input_dir+"point_to_coord_val.npy",
+                months=self.months,
+                skip_samples = self.hparams.skip_samples,
+                return_size=self.hparams.return_size,
+                local_multi_sampling=self.hparams.local_multi_sampling,
+                whiten_with_pca=self.hparams.whiten_with_pca,
+            )"""
+            _, self.data_val, _ = torch.utils.data.random_split(self.data_train, [0.9, 0.05, 0.05])
+
+        if stage == "test" and not self.data_test:
+            """self.data_test = ChelsaCLIPDataset(
+                monthly_arrays=self.monthly_arrays,
+                land_coordinates_file=self.hparams.input_dir+"land_coordinates_test.npy",
+                point_to_coord_file=self.hparams.input_dir+"point_to_coord_test.npy",
+                months=self.months,
+                skip_samples = self.hparams.skip_samples,
+                return_size=self.hparams.return_size,
+                local_multi_sampling=self.hparams.local_multi_sampling,
+                whiten_with_pca=self.hparams.whiten_with_pca,
+            )"""
+            _, _, self.data_test = torch.utils.data.random_split(self.data_train, [0.9, 0.05, 0.05])
 
     def train_dataloader(self) -> DataLoader[Any]:
         """Create and return the train dataloader.
 
         :return: The train dataloader.
         """
+        if self.hparams.sampler == "rw_samp":
+            weights = np.load(self.hparams.input_dir+"idx_to_weight.npy")
+            if self.hparams.skip_samples:
+                weights = weights[::self.hparams.skip_samples]
+            if not self.hparams.use_all_for_training:
+                raise ValueError("Can't use weighted random sampling with train sub-set. Need to set data.use_all_for_training: true")
+            sampler = torch.utils.data.WeightedRandomSampler(
+                weights=weights,
+                num_samples=len(self.data_train),
+                replacement=True,
+                generator=None)
+            shuffle = False
+        elif self.hparams.sampler == "rw_samp_world":
+            weights = np.load(self.hparams.input_dir+"idx_to_weight.npy")
+            if self.hparams.skip_samples:
+                weights = weights[::self.hparams.skip_samples]
+            if not self.hparams.use_all_for_training:
+                raise ValueError("Can't use weighted random sampling with train sub-set. Need to set data.use_all_for_training: true")
+            sampler = WeightedRandomWorldSampler(weights, len(self.data_train))
+            shuffle = False
+        else:
+            sampler = None
+            shuffle = True
         return DataLoader(
             dataset=self.data_train,
             batch_size=self.batch_size_per_device,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
-            shuffle=True,
+            shuffle=shuffle,
+            collate_fn=self.collate_fn,
+            sampler=sampler
         )
 
     def val_dataloader(self) -> DataLoader[Any]:
@@ -126,12 +246,15 @@ class ChelsaCLIPDataModule(LightningDataModule):
 
         :return: The validation dataloader.
         """
+        # IMPORTANT: shuffle needs to be True, as the loss is sensitive to the batch composition.
+        # Thus we need to match the random composition of the training batches in the validation and test data
         return DataLoader(
             dataset=self.data_val,
             batch_size=self.batch_size_per_device,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
-            shuffle=False,
+            shuffle=True,
+            collate_fn=self.collate_fn
         )
 
     def test_dataloader(self) -> DataLoader[Any]:
@@ -144,7 +267,8 @@ class ChelsaCLIPDataModule(LightningDataModule):
             batch_size=self.batch_size_per_device,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
-            shuffle=False,
+            shuffle=True,
+            collate_fn=self.collate_fn
         )
 
     def teardown(self, stage: Optional[str] = None) -> None:
@@ -173,4 +297,7 @@ class ChelsaCLIPDataModule(LightningDataModule):
 
 
 if __name__ == "__main__":
-    _ = ChelsaCLIPDataModule()
+    _ = ChelsaCLIPDataModule(
+        climatology_dir="/shares/wegner.ics.uzh/CHELSA/climatologies/1981-2010_numpy/",
+        input_dir="/shares/wegner.ics.uzh/CHELSA/input/",
+        batch_size=32000)
--- src/data/chelsaCLIP_dataset.py
+++ src/data/chelsaCLIP_dataset.py
@@ -1,90 +1,185 @@
 from torch.utils.data import Dataset
 from time import time
-from collections import defaultdict
 from tqdm import tqdm
+import pickle as pk
 import numpy as np
 import os
 import json
 import rioxarray
 import random
+import torch
+import random
 
+from typing import List
 
 class ChelsaCLIPDataset(Dataset):
-    def __init__(self, climatology_dir: str, land_coordinates_file: str, use_bio: bool=False, use_dem: bool=False, verbose: bool=False):
+    def __init__(
+        self,
+        monthly_arrays,
+        land_coordinates_file: str,
+        point_to_coord_file: str,
+        months: List[str],
+        skip_samples: int,
+        return_size: int,
+        local_multi_sampling: bool,
+        whiten_with_pca: bool,
+        ):
         
-        # Load the 11*12 monthly rasters
-        start = time()
-        
-        self.var_names = ["clt", "cmi", "hurs", "pet", "pr", "rsds", "sfcWind", "tas", "tasmax", "tasmin", "vpd"]
-        self.months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-        self.rasters = defaultdict(list)
-        for month in tqdm(self.months):
-            for var in self.var_names:
-                if var == "pet":
-                    raster_file_name = climatology_dir + var + "/CHELSA_pet_penman_" + str(month) + "_1981-2010_V.2.1.tif"
-                elif var == "rsds":
-                    raster_file_name = climatology_dir + var + "/CHELSA_" + var + "_1981-2010_" + str(month) + "_V.2.1.tif"
-                else:
-                    raster_file_name = climatology_dir + var + "/CHELSA_" + var + "_" + str(month) + "_1981-2010_V.2.1.tif"
-                raster = rioxarray.open_rasterio(raster_file_name)
-                self.rasters[month].append(raster)
-        self.x_to_lon = raster.indexes["x"]
-        self.y_to_lat = raster.indexes["y"]
-        if verbose:
-            print("Loaded monthly rasters in:", time() - start)
+        self.monthly_arrays = monthly_arrays
+        if return_size > 1:
+            self.y_len, self.x_len = self.monthly_arrays[list(self.monthly_arrays.keys())[0]][0].shape
 
-        if use_bio:
-            raise NotImplementedError()
+        self.months = months
 
-        if use_dem:
-            raise NotImplementedError()
+        self.return_size = return_size
+
+        self.local_multi_sampling = local_multi_sampling
         
+        if local_multi_sampling:
+            ras = rioxarray.open_rasterio('/shares/wegner.ics.uzh/CHELSA/climatologies/1981-2010/cmi/CHELSA_cmi_01_1981-2010_V.2.1.tif', cache=False)
+            if "Switzerland" in land_coordinates_file:
+                NW_y, NW_x = (4340, 22310)
+                SE_y, SE_x = (4585, 22860)
+                ras = ras[:,NW_y:SE_y,NW_x:SE_x]
+            self.x_to_lon = ras["x"]
+            self.y_to_lat = ras["y"]
+            ras.close()
+
+        if whiten_with_pca:
+            self.whiten_pca =  pk.load(open("/shares/wegner.ics.uzh/CHELSA/input/whiten_pca_components.pkl",'rb')) 
+        else:
+            self.whiten_pca = None
+
         # Load location file
-        start = time()
-        self.locs = np.load(land_coordinates_file)
-        if verbose:
-            print("Loaded locations in:", time() - start)
+        if skip_samples is None:
+            #self.locs = np.load(land_coordinates_file)
+            self.point_to_coord = np.load(point_to_coord_file)
+        else:
+            #self.locs = np.load(land_coordinates_file)[::skip_samples]
+            self.point_to_coord = np.load(point_to_coord_file)[::skip_samples]
+            for month in self.monthly_arrays.keys():
+                self.monthly_arrays[month][:, ::skip_samples]
 
     def __len__(self):
-        return len(self.locs)
+        return len(self.point_to_coord)
+
+    def get_idx(self, idx, month):
+        #pixel_y, pixel_x = self.locs[idx]
+        lonlat = self.point_to_coord[idx]
+
+        if self.return_size > 1:
+            side = self.return_size // 2
+            if pixel_y-side < 0:
+                y_left = 0
+                y_right = self.return_size
+            elif pixel_y+side >= self.y_len:
+                y_left = self.y_len - self.return_size
+                y_right = self.y_len
+            else:
+                y_left = pixel_y-side
+                y_right = pixel_y+side+1
+            if pixel_x-side < 0:
+                x_up = 0
+                x_down = self.return_size
+            elif pixel_x+side >= self.x_len:
+                x_up = self.x_len - self.return_size
+                x_down = self.x_len
+            else:
+                x_up = pixel_x-side
+                x_down = pixel_x+side+1
+            chelsa = self.monthly_arrays[month][:, y_left:y_right, x_up:x_down]
+        else:
+            #chelsa = self.monthly_arrays[month][:, pixel_y, pixel_x]
+            chelsa = self.monthly_arrays[month][:, idx]
+            #print(pixel_y, pixel_x, self.monthly_arrays[month].shape, self.locs.shape)
+
+        if self.whiten_pca:
+            chelsa = self.whiten_pca.transform(chelsa.reshape((1,11))).reshape((11)).astype("float16")
+
+        return lonlat, int(month), chelsa
+
+    """def get_idx(self, idx):
+        pixel_y, pixel_x = self.locs[idx]
+        lonlat = self.point_to_coord[idx]
+
+        if self.return_size > 1:
+            raise NotImplementedError()
+        else:
+            chelsa = []
+            for mar in self.monthly_arrays.values():
+                chelsa.append(torch.tensor(mar[:, pixel_y, pixel_x]))
+        chelsa = torch.cat(chelsa)
+
+        if self.whiten_pca:
+            chelsa = self.whiten_pca.transform(chelsa.reshape((1,11))).reshape((11)).astype("float16")
+
+        return lonlat, chelsa"""
+
+    def get_random_adjacent_to_idx(self, idx, month):
+        pixel_y, pixel_x = self.locs[idx]
+
+        # Calculate adjacent pixel, and make sure it is in bounds
+        # Repeat
+        y, x = pixel_y, pixel_x
+        while(y == pixel_y and x == pixel_x):
+            y, x = pixel_y + random.randint(-1, 1), pixel_x + random.randint(-1, 1)
+            y, x = max(y, 0), max(x, 0)
+            shape = self.monthly_arrays[month][0].shape
+            y, x = min(y, shape[0]-1), min(x, shape[1]-1)
+
+        chelsa = self.monthly_arrays[month][:, y, x]
+        lonlat = np.array([self.x_to_lon[x], self.y_to_lat[y]])
+        return lonlat, int(month), chelsa
+    
+    def get_all_adjacent_to_idx(self, idx, month):
+        pixel_y, pixel_x = self.locs[idx]
+
+        # Calculate all adjacent pixel, and make sure they are in bounds
+        lonlats, months, chelsas = [], [], []
+        shape = self.monthly_arrays[month][0].shape
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                y, x = pixel_y + i, pixel_x + j
+                if not (y < 0 or y > shape[0]-1 or x < 0 or x > shape[1]-1 or (x == pixel_x and y == pixel_y)):
+                    lonlats.append(torch.tensor(np.array([self.x_to_lon[x], self.y_to_lat[y]])))
+                    months.append(torch.tensor(int(month)))
+                    chelsas.append(torch.tensor(self.monthly_arrays[month][:, y, x]))
+        return lonlats, months, chelsas
 
     def __getitem__(self, idx):
-        pixel_y = self.locs[idx, 0]
-        pixel_x = self.locs[idx, 1]
-
-        lat = self.y_to_lat[pixel_y]
-        lon = self.x_to_lon[pixel_x]
-
-        month = random.choice(self.months)
-
-        chelsa = []
-        for raster in self.rasters[month]:
-            print(raster)
-            chelsa.append(raster[0, pixel_x, pixel_y])
-        chelsa = np.stack(chelsa)
-
-        return (lon, lat, month), chelsa
+        if self.local_multi_sampling:
+            month = random.choice(self.months)
+            l1, m1, c1 = self.get_idx(idx, month)
+            mode = "other" #"random_adjacent"
+            if mode == "random_adjacent":
+                l2, m2, c2 = self.get_random_adjacent_to_idx(idx, month)
+                lonlats = torch.stack([torch.tensor(l1), torch.tensor(l2)])
+                months = torch.stack([torch.tensor(m1), torch.tensor(m2)])
+                chelsas = torch.stack([torch.tensor(c1), torch.tensor(c2)])
+            else:
+                lonlats, months, chelsas = self.get_all_adjacent_to_idx(idx, month)
+                lonlats.append(torch.tensor(l1))
+                months.append(torch.tensor(m1))
+                chelsas.append(torch.tensor(c1))
+                lonlats = torch.stack(lonlats)
+                months = torch.stack(months)
+                chelsas = torch.stack(chelsas)
+            return lonlats, months, chelsas
+        else:
+            return self.get_idx(idx, month = random.choice(self.months))
+            #return self.get_idx(idx)
 
 
 if __name__ == "__main__":
     from time import time
     start = time()
     ds = ChelsaCLIPDataset(
-        climatology_dir="/shares/wegner.ics.uzh/CHELSA/climatologies/1981-2010_normalized/",
+        climatology_dir="/shares/wegner.ics.uzh/CHELSA/climatologies/1981-2010_numpy/",
         land_coordinates_file="/shares/wegner.ics.uzh/CHELSA/input/land_coordinates_train.npy",
-        verbose=True)
+        loc_to_coord_file="/shares/wegner.ics.uzh/CHELSA/input/point_to_coord.npy")
     print(" -> Total loading time:", time() - start)
     print("Dataset of Len:", len(ds))
 
-    for i in tqdm(range(32000)):
-        ds[i*1000]
-
-
-
-    # Current issues: It is way to slow in loading
-    #   There is one raster that is smaller
-    # -> Make the files smaller through datatype (currently float64), or load them smaller -> Can load them all into RAM
-    # -> Sample the one smaller raster by lat/lon instead
-    # -> Or maybe convert it to the bigger size first and then sample it normally
-    # -> Why does my normalization code not properly delete stuff after looking at the rasters??
+    for i in tqdm(range(len(ds))):
+        ds[i]
 
